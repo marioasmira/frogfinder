ef5f46164996ccfaeb19da19ed9f9399a544ca52
Mario S. Mira <marioasmira@gmail.com>
2020-09-21 18:33:41 +0200

    small changes in style

--- pi_frog.py
+++ pi_frog.py
@@ -3,11 +3,11 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import argparse
 import warnings
-import datetime
 import imutils
 import json
 import time
 import cv2
+import sys
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -15,60 +15,64 @@ ap.add_argument("-c", "--conf", required=True,
             help="path to the JSON configuration file")
 args = vars(ap.parse_args())
 
-# filter warnings, load the configuration and initialize the Dropbox
-# client
-warnings.filterwarnings("ignore")
+# filter warnings
+if not sys.warnoptions:
+    warnings.filterwarnings("ignore")
+
+# import configuration options
 conf = json.load(open(args["conf"]))
 
 # initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = tuple(conf["detection_resolution"])
-camera.framerate = conf["detection_fps"]
-rawCapture = PiRGBArray(camera, size=tuple(conf["detection_resolution"]))
+cam = PiCamera()
+cam.resolution = tuple(conf["detection_resolution"])
+cam.framerate = conf["detection_fps"]
+raw_capture = PiRGBArray(cam, size=tuple(conf["detection_resolution"]))
 video = conf["video_path"]
-# allow the camera to warmup, then initialize the average frame, last
-# uploaded timestamp, and frame motion counter
-print("[INFO] warming up...")
+
+# allow the camera to warmup, then initialize the average frame, and frame motion counter
+print("[INFO] Warming up...")
 time.sleep(conf["camera_warmup_time"])
 avg = None
-motionCounter = 0
+motion_counter = 0
 
 # set up data file
 data_file = open((time.strftime("%Y%m%d_%H%M%S") + "_" +
     str(conf["min_motion_frames"]) + "_" +
     str(conf["min_area"]) + ".csv"), "w+")
-data_file.write("time,motionCounter,iter,contour\n")
+data_file.write("time,motion_counter,iter,contour\n")
 if conf["debug"]:
-    print("time,motionCounter,iter,contour\n")
+    print("time    motion_counter    iter    contour\n")
 
 # start loop
 while True:
     #capture frames from the camera
-    for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    for f in cam.capture_continuous(raw_capture, format="bgr", use_video_port=True):
         # grab the raw NumPy array representing the image and initialize
         # the timestamp and occupied/unoccupied text
         frame = f.array
-        text = "No frog"
+        presence = False
+
         # resize the frame, convert it to grayscale, and blur it
         frame = imutils.resize(frame, width=500)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
         # if the average frame is None, initialize it
         if avg is None:
-            print("[INFO] starting background model...")
+            print("[INFO] Starting background model...")
             avg = gray.copy().astype("float")
-            rawCapture.truncate(0)
+            raw_capture.truncate(0)
             continue
 
         # accumulate the weighted average between the current frame and
         # previous frames, then compute the difference between the current
         # frame and running average
         cv2.accumulateWeighted(gray, avg, 0.5)
-        frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+        frame_delta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
         # threshold the delta image, dilate the thresholded image to fill
         # in holes, then find contours on thresholded image
-        thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255,
+        thresh = cv2.threshold(frame_delta, conf["delta_thresh"], 255,
             cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations=2)
         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
@@ -77,61 +81,58 @@ while True:
 
 
         # loop over the contours
-        counter = 0
+        counter = 0 # to print how many areas were picked up as motion
+        frame_time = time.strftime("%Y%m%d_%H%M%S%f")   # each frame can have more than one area
         if len(cnts) < conf["max_areas"]:
             for c in cnts:
-                data_file.write(time.strftime("%Y%m%d_%H%M%S") + "," +
-                        str(motionCounter) + "," +
+                data_file.write(frame_time + "," +
+                        str(motion_counter) + "," +
                         str(counter) + "," +
                         str(cv2.contourArea(c)) + "\n")
                 if conf["debug"]:
-                        print(time.strftime("%Y%m%d_%H%M%S") + "    " +
-                            str(motionCounter) + "    " +
+                        print(frame_time + "    " +
+                            str(motion_counter) + "    " +
                             str(counter) + "    " +
                             str(cv2.contourArea(c)))
                 # if the contour is too small, ignore it
                 counter += 1
                 if ((cv2.contourArea(c) > conf["min_area"]) and (cv2.contourArea(c) < conf["max_area"])):
                     # and update the text
-                    text = "Frog!"
+                    presence = True
                 else:
                     continue
 
-        if text == "Frog!":
-            motionCounter += 1
-            # check to see if the number of frames with consistent motion is
-            # high enough
-            if (motionCounter >= conf["min_motion_frames"]):
-                print("Got one!")
+        if presence:
+            motion_counter += 1
+            # check to see if the number of frames with consistent motion is high enough
+            if (motion_counter >= conf["min_motion_frames"]):
+                print("[INFO] Got one!")
                 break
         else:
-            motionCounter = 0
+            motion_counter = 0
 
         # clear the stream in preparation for the next frame
-        rawCapture.truncate(0)
+        raw_capture.truncate(0)
 
 
     # change resolution and framerate for HD
     print("[INFO] Changing camera resolution and framerate...")
-    camera.resolution = tuple(conf["capture_resolution"])
-    camera.framerate = conf["capture_fps"]
+    cam.resolution = tuple(conf["capture_resolution"])
+    cam.framerate = conf["capture_fps"]
     video_name = video + time.strftime("%Y%m%d_%H%M%S") + ".h264"
 
     # record video
     print("[INFO] Start recording.")
-    camera.start_recording(video_name)
-    camera.wait_recording(conf["upload_seconds"])
-    camera.stop_recording()
+    cam.start_recording(video_name)
+    cam.wait_recording(conf["upload_seconds"])
+    cam.stop_recording()
     print("[INFO] Finished recording!")
     print("[INFO] Returning camera to search values.")
     
     # return values to originals
-    camera.resolution = tuple(conf["detection_resolution"])
-    camera.framerate = conf["detection_fps"]
-    rawCapture = PiRGBArray(camera, size=tuple(conf["detection_resolution"]))
+    cam.resolution = tuple(conf["detection_resolution"])
+    cam.framerate = conf["detection_fps"]
+    raw_capture = PiRGBArray(cam, size=tuple(conf["detection_resolution"]))
     motionCounter = 0
     avg = None
-
-    # warmup to new resolution
-    time.sleep(conf["camera_warmup_time"])
 
