4718312fdcb87a518b9c6b30cdb316e1234037ee
Mario S. Mira <marioasmira@gmail.com>
2020-11-16 16:49:11 +0100

    changed physical display so now it works with common anode

--- frogutils/displayhandle.py
+++ frogutils/displayhandle.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 # each list displays one digit from 0-9
+# firs for common cathode displays
+"""
 digit_array = [[0,0,0,0,0,0,1],\
         [1,0,0,1,1,1,1],\
         [0,0,1,0,0,1,0],\
@@ -12,6 +14,19 @@ digit_array = [[0,0,0,0,0,0,1],\
         [0,0,0,1,1,1,1],\
         [0,0,0,0,0,0,0],\
         [0,0,0,0,1,0,0]]
+"""
+# second for common anode displays
+digit_array = [[1,1,1,1,1,1,0],\
+        [0,1,1,0,0,0,0],\
+        [1,1,0,1,1,0,1],\
+        [1,1,1,1,0,0,1],\
+        [0,1,1,0,0,1,1],\
+        [1,0,1,1,0,1,1],\
+        [1,0,1,1,1,1,1],\
+        [1,1,1,0,0,0,0],\
+        [1,1,1,1,1,1,1],\
+        [1,1,1,1,0,1,1]]
+
 
 def display(conf, val1, val2, wait_time):
     # turn the two values into a list
@@ -27,20 +42,20 @@ def display(conf, val1, val2, wait_time):
 
     # reset display
     for pin in conf["display_pins"]:
-        GPIO.output(pin,True)
-    for pin in conf["digit_pins"]:
         GPIO.output(pin,False)
-    GPIO.output(conf["display_dot_pin"], True)
+    for pin in conf["digit_pins"]:
+        GPIO.output(pin,True)
+    GPIO.output(conf["display_dot_pin"], False)
 
     # the number 50 comes from 200 / 4
     # 200 would make the loop run for the same length as the conf file
     # but since there are 4 digits to display it gets divided by 4 to keep the interval equal to the conf file
     for n in range(1, (wait_time * 50)):
         for pos in range(0, 4):
-            GPIO.output(conf["digit_pins"][pos], True)
+            GPIO.output(conf["digit_pins"][pos], False)
             val = digit_list[pos]
 
             for led in range(0, 7):
                 GPIO.output(conf["display_pins"][led], digit_array[val][led])
             time.sleep(0.005)
-            GPIO.output(conf["digit_pins"][pos], False)
+            GPIO.output(conf["digit_pins"][pos], True)
